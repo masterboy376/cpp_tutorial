1f030cd69c35b42dae0739dc49363e013ad19859
Sambhav Kaushik <92575005+masterboy376@users.noreply.github.com>
2022-08-15 12:52:58 +0000

    polymorphism, this pointer, pointer derived class

---
+++ polymorphism.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    // compile-time eg: function overloading, operator overloading --> binding occurs
+    // run-time eg: virtual overloading
+
+    return 0;
+
+}
